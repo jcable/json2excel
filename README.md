b2ced3e28800a5af372e71b620c7af940939f398
jcable <julian.cable@yahoo.com>
2018-09-28 23:35:06 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # json2excel
 Apache Camel context to poll a folder for JSON documents and create Excel workbooks
+
+To build this project use
+
+    mvn install
+
+To run this project from within Maven use
+
+    mvn exec:java
+
+For more help see the Apache Camel documentation
+
+    http://camel.apache.org/
+
---
+++ src/main/java/wainteg/json2excel/MainApp.java
@@ -0,0 +1,21 @@
+package wainteg.json2excel;
+
+import org.apache.camel.main.Main;
+
+/**
+ * A Camel Application
+ */
+public class MainApp {
+
+    /**
+     * A main() so we can easily run these routing rules in our IDE
+     */
+    public static void main(String... args) throws Exception {
+
+        Main main = new Main();
+        main.addRouteBuilder(new MyRouteBuilder());
+        main.run(args);
+    }
+
+}
+
