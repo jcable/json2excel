f3262cb0302f5fa432b15d09fb56972f66e45de7
Julian Cable <julian.cable@yahoo.com>
2018-09-28 23:37:11 +0100

    Update MyRouteBuilder.groovy

--- src/main/groovy/wainteg/json2excel/MyRouteBuilder.groovy
+++ src/main/groovy/wainteg/json2excel/MyRouteBuilder.groovy
@@ -21,41 +21,41 @@ class MyRouteBuilder extends RouteBuilder {
         .to('log:start')
         .unmarshal(format)
         .process{
-        		Workbook wb = new XSSFWorkbook()
-		    Font headingFont = wb.createFont();
-		    headingFont.setBold(true);
-			XSSFCellStyle headingStyle = wb.createCellStyle();
-			headingStyle.setFont(headingFont);
-        		it.in.body.each{ k,v -> 
-	    			def rows = v
-	    			if(k == 'broadcasts') rows = rows.broadcasts[0]
-	        		XSSFSheet sheet = (XSSFSheet) wb.createSheet(k)
-	        		def r = 0
-	    			Row row = sheet.createRow(r++)
-		        row.setRowStyle(headingStyle);
-	    			def c = 0
-	        		def headings = rows.collectMany{it.keySet()}.toSet().toArray()
-	        		headings.each{
+        	Workbook wb = new XSSFWorkbook()
+		Font headingFont = wb.createFont();
+		headingFont.setBold(true);
+		XSSFCellStyle headingStyle = wb.createCellStyle();
+		headingStyle.setFont(headingFont);
+		it.in.body.each{ k,v -> 
+			def rows = v
+			if(k == 'broadcasts') rows = rows.broadcasts[0]
+			XSSFSheet sheet = (XSSFSheet) wb.createSheet(k)
+			def r = 0
+			Row row = sheet.createRow(r++)
+			row.setRowStyle(headingStyle);
+			def c = 0
+			def headings = rows.collectMany{it.keySet()}.toSet().toArray()
+			headings.each{
+				Cell cell = row.createCell(c++);
+				cell.setCellValue(it)
+			}
+			rows.each{ p ->
+				row = sheet.createRow(r++)
+				c = 0
+				headings.each{ h ->
 					Cell cell = row.createCell(c++);
-					cell.setCellValue(it)
-	        		}
-	        		rows.each{ p ->
-	        			row = sheet.createRow(r++)
-	        			c = 0
-	        			headings.each{ h ->
-						Cell cell = row.createCell(c++);
-						if(p.containsKey(h)) {
-							cell.setCellValue(p[h])	        				
-						} else {
-							cell.setCellValue(' ')	        				
-						}
-	        			}
-	        		}	        		
-	        		while(--c >= 0)
-				    sheet.autoSizeColumn(c);	        		
-        		}
-    			OutputStream fileOut = new FileOutputStream("workbook.xlsx")
-    			wb.write(fileOut)
+					if(p.containsKey(h)) {
+						cell.setCellValue(p[h])	        				
+					} else {
+						cell.setCellValue(' ')	        				
+					}
+				}
+			}	        		
+			while(--c >= 0)
+			    sheet.autoSizeColumn(c);	        		
+		}
+		OutputStream fileOut = new FileOutputStream("workbook.xlsx")
+		wb.write(fileOut)
         }        
         .to('log:end')
     }
